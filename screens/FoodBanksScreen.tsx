ac3f53ffaf7ce2b68e4aed45f966f95263e76548
filippofinke <filippofinke@protonmail.ch>
2023-02-21 15:35:11 +0100

    feat: add user location

--- screens/FoodBanksScreen.tsx
+++ screens/FoodBanksScreen.tsx
@@ -1,14 +1,40 @@
 import { StyleSheet } from "react-native";
 
 import { View } from "../components/Themed";
-import React from "react";
-import MapView, { Geojson } from "react-native-maps";
+import React, { useEffect } from "react";
+import MapView, { Geojson, Region } from "react-native-maps";
 import * as FoodBanks from "../assets/foodbanks.json";
+import * as Location from "expo-location";
 
 export default function FoodBanksScreen() {
+  const ref = React.useRef<MapView>(null);
+
+  useEffect(() => {
+    (async () => {
+      let result = await Location.requestForegroundPermissionsAsync();
+      if (result.granted) {
+        let location = await Location.getCurrentPositionAsync({
+          accuracy: Location.Accuracy.High,
+        });
+
+        ref.current?.animateToRegion({
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        });
+      }
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map}>
+      <MapView
+        ref={ref}
+        style={styles.map}
+        showsUserLocation={true}
+        showsMyLocationButton={true}
+      >
         <Geojson
           geojson={FoodBanks as GeoJSON.FeatureCollection<GeoJSON.Point>}
           strokeColor="red"
