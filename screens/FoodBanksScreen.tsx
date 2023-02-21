ba0f6fd371a2524436f004ca4fac4be551cf567e
filippofinke <filippofinke@protonmail.ch>
2023-02-21 14:52:40 +0100

    feat: show foodbanks on the map

--- screens/FoodBanksScreen.tsx
+++ screens/FoodBanksScreen.tsx
@@ -2,12 +2,20 @@ import { StyleSheet } from "react-native";
 
 import { View } from "../components/Themed";
 import React from "react";
-import MapView from "react-native-maps";
+import MapView, { Geojson } from "react-native-maps";
+import * as FoodBanks from "../assets/foodbanks.json";
 
 export default function FoodBanksScreen() {
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView style={styles.map}>
+        <Geojson
+          geojson={FoodBanks as GeoJSON.FeatureCollection<GeoJSON.Point>}
+          strokeColor="red"
+          fillColor="green"
+          strokeWidth={2}
+        />
+      </MapView>
     </View>
   );
 }
