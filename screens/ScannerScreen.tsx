1c95fc9e0fd244309aee8bd39e1de73383c3e889
filippofinke <filippofinke@protonmail.ch>
2023-02-22 09:40:28 +0100

    feat: show data from product

--- screens/ScannerScreen.tsx
+++ screens/ScannerScreen.tsx
@@ -1,15 +1,18 @@
-import { Alert, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 import { Text, View } from "../components/Themed";
 import { BarCodeScanner } from "expo-barcode-scanner";
-import React, { useState, useEffect } from "react";
-import { Button } from "native-base";
+import { Button, HStack, Image, VStack } from "native-base";
 import { OpenFoodFactsApi } from "openfoodfac-ts";
+import { Product } from "openfoodfac-ts/dist/OpenFoodFactsApi/types";
+import React, { useState, useEffect } from "react";
+import BottomSheet from "@gorhom/bottom-sheet";
 
 const openFoodFactsApi = new OpenFoodFactsApi();
 
 export default function ScannerScreen() {
-  const [hasPermission, setHasPermission] = useState(null);
-  const [scanned, setScanned] = useState(false);
+  const [hasPermission, setHasPermission] = useState<boolean>(false);
+  const [scanned, setScanned] = useState<boolean>(false);
+  const [product, setProduct] = useState<Product | null>(null);
 
   useEffect(() => {
     const getBarCodeScannerPermissions = async () => {
@@ -29,11 +32,12 @@ export default function ScannerScreen() {
   }) => {
     setScanned(true);
 
+    console.log(
+      `Bar code with type ${type} and data ${data} has been scanned!`
+    );
+
     openFoodFactsApi.findProductByBarcode(data).then((product) => {
-      Alert.alert(
-        "Product found",
-        product?.product_name || "Product not found"
-      );
+      setProduct(product);
     });
   };
 
@@ -46,24 +50,50 @@ export default function ScannerScreen() {
 
   return (
     <View style={styles.container}>
-      <Button
-        style={{
-          position: "absolute",
-          bottom: 30,
-          zIndex: 1,
-        }}
-        onPress={() => {
-          // TODO: Add manual product
-        }}
-      >
-        Add manually
-      </Button>
+      {!product && (
+        <Button
+          style={{
+            position: "absolute",
+            bottom: 30,
+            zIndex: 1,
+          }}
+          onPress={() => {
+            // TODO: Add manual product
+          }}
+        >
+          Add manually
+        </Button>
+      )}
+
       <BarCodeScanner
         onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
         style={StyleSheet.absoluteFillObject}
       />
-      {scanned && (
-        <Button onPress={() => setScanned(false)}>Tap to Scan Again</Button>
+
+      {product && (
+        <BottomSheet
+          enablePanDownToClose={true}
+          snapPoints={["50%", "100%"]}
+          index={0}
+          onClose={() => {
+            setScanned(false);
+          }}
+        >
+          <HStack>
+            <Image
+              src={product.image_url}
+              alt={product.product_name}
+              resizeMode={"contain"}
+              size={"xl"}
+            />
+            <VStack>
+              <Text style={styles.title}>{product.product_name}</Text>
+              {product.ingredients?.map((ingredient) => (
+                <Text style={styles.ingredients}>{ingredient.text}</Text>
+              ))}
+            </VStack>
+          </HStack>
+        </BottomSheet>
       )}
     </View>
   );
@@ -78,6 +108,9 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: "bold",
+  },
+  ingredients: {
+    fontSize: 15,
   },
   separator: {
     marginVertical: 30,
