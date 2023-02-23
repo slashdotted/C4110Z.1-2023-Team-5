acbf13f803f74b604d4f31ba115cf6c7c972d827
matteoforni <matteoforni@hotmail.com>
2023-02-23 13:38:53 +0100

    added delete feature

--- screens/FridgeScreen.tsx
+++ screens/FridgeScreen.tsx
@@ -6,85 +6,23 @@ import { RootTabScreenProps } from "../types";
 import { FlatList, TouchableOpacity } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
 import Swipeable from "react-native-gesture-handler/Swipeable";
-import { Pressable } from "native-base";
+import { Pressable, Center } from "native-base";
 
-type ItemData = {
+type Items = {
   id: string;
   title: string;
   expirationDate: string;
   level: number;
 };
 
-const DATA: ItemData[] = [
-  {
-    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-    title: "First Item",
-    expirationDate: "02.03.2023",
-    level: 1,
-  },
-  {
-    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
-    title: "Second Item",
-    expirationDate: "02.03.2023",
-    level: 1,
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-145571e29d72",
-    title: "Third Item",
-    expirationDate: "03.03.2023",
-    level: 1,
-  },
-  {
-    id: "3ac68afc-c605-48d3-a4f8-fbd9321aa97f63",
-    title: "Fourth Item",
-    expirationDate: "04.03.2023",
-    level: 1,
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-1455as71e29d72",
-    title: "Fifth Item",
-    expirationDate: "05.03.2023",
-    level: 1,
-  },
-  {
-    id: "bd7acbea-c1b1-46c2-aed5-3ad553abb28ba",
-    title: "Sixth Item",
-    expirationDate: "06.03.2023",
-    level: 1,
-  },
-  {
-    id: "3ac68afc-c605-48d3-a4f8-fbd941aa97f63",
-    title: "Seventh Item",
-    expirationDate: "07.03.2023",
-    level: 2,
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-1453571e29d72",
-    title: "Eight Item",
-    expirationDate: "08.03.2023",
-    level: 2,
-  },
-  {
-    id: "3ac68afc-c605-48d3-a4f8-fbd29321aa97f63",
-    title: "Ninth Item",
-    expirationDate: "09.03.2023",
-    level: 3,
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-14515as71e29d72",
-    title: "Tenth Item",
-    expirationDate: "10.03.2023",
-    level: 3,
-  },
-];
-
 type ItemProps = {
-  item: ItemData;
+  item: Items;
   onPress: () => void;
+  onDelete: (item: Items) => void;
   textColor: string;
 };
 
-const Item = ({ item, onPress, textColor }: ItemProps) => {
+const Item = ({ item, onPress, textColor, onDelete }: ItemProps) => {
   let style = {
     borderColor: "#B91C1C",
   };
@@ -105,7 +43,7 @@ const Item = ({ item, onPress, textColor }: ItemProps) => {
         <Pressable
           px={3}
           onPress={() => {
-            // TODO: Delete item
+            onDelete(item)
           }}
           justifyContent={"center"}
           bg={"red.500"}
@@ -138,9 +76,72 @@ const Item = ({ item, onPress, textColor }: ItemProps) => {
 export default function FridgeScreen({
   navigation,
 }: RootTabScreenProps<"Fridge">) {
+  const [items, setItems] = useState<Items[]>([
+    {
+      id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+      title: "First Item",
+      expirationDate: "02.03.2023",
+      level: 1,
+    },
+    {
+      id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+      title: "Second Item",
+      expirationDate: "02.03.2023",
+      level: 1,
+    },
+    {
+      id: "58694a0f-3da1-471f-bd96-145571e29d72",
+      title: "Third Item",
+      expirationDate: "03.03.2023",
+      level: 1,
+    },
+    {
+      id: "3ac68afc-c605-48d3-a4f8-fbd9321aa97f63",
+      title: "Fourth Item",
+      expirationDate: "04.03.2023",
+      level: 1,
+    },
+    {
+      id: "58694a0f-3da1-471f-bd96-1455as71e29d72",
+      title: "Fifth Item",
+      expirationDate: "05.03.2023",
+      level: 1,
+    },
+    {
+      id: "bd7acbea-c1b1-46c2-aed5-3ad553abb28ba",
+      title: "Sixth Item",
+      expirationDate: "06.03.2023",
+      level: 1,
+    },
+    {
+      id: "3ac68afc-c605-48d3-a4f8-fbd941aa97f63",
+      title: "Seventh Item",
+      expirationDate: "07.03.2023",
+      level: 2,
+    },
+    {
+      id: "58694a0f-3da1-471f-bd96-1453571e29d72",
+      title: "Eight Item",
+      expirationDate: "08.03.2023",
+      level: 2,
+    },
+    {
+      id: "3ac68afc-c605-48d3-a4f8-fbd29321aa97f63",
+      title: "Ninth Item",
+      expirationDate: "09.03.2023",
+      level: 3,
+    },
+    {
+      id: "58694a0f-3da1-471f-bd96-14515as71e29d72",
+      title: "Tenth Item",
+      expirationDate: "10.03.2023",
+      level: 3,
+    },
+  ]);
+
   const [selectedId, setSelectedId] = useState<string>();
 
-  const renderItem = ({ item }: { item: ItemData }) => {
+  const renderItem = ({ item }: { item: Items }) => {
     const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
     const color = item.id === selectedId ? "grey" : "black";
 
@@ -148,6 +149,9 @@ export default function FridgeScreen({
       <Item
         item={item}
         onPress={() => setSelectedId(item.id)}
+        onDelete={(item) => setItems((prev) =>
+          prev.filter((n) => n.id !== item.id)
+        )}
         textColor={color}
       />
     );
@@ -156,10 +160,15 @@ export default function FridgeScreen({
   return (
     <View style={styles.container}>
       <FlatList
-        data={DATA}
+        data={items}
         renderItem={renderItem}
         keyExtractor={(item) => item.id}
         extraData={selectedId}
+        ListEmptyComponent={
+          <Center>
+            <Text>There are no items in your Fridge</Text>
+          </Center>
+        }
       />
     </View>
   );
