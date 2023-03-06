9f6d364e4e7d83d59fe4de50f48d060f81b02110
filippofinke <filippofinke@protonmail.ch>
2023-03-06 11:50:24 +0100

    feat: create notificationsReducer #20

---
+++ storage/reducers/notificatonsReducer.ts
@@ -0,0 +1,41 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+import { Notification } from "../../constants/Types";
+
+export interface NotificationsState {
+  notifications: Notification[];
+}
+
+const initialState: NotificationsState = {
+  notifications: [
+    {
+      id: 1,
+      title: "Notification 1",
+      status: "success",
+      date: new Date(),
+    },
+  ],
+};
+
+export const notificationsSlice = createSlice({
+  name: "notifications",
+  initialState,
+  reducers: {
+    addNotification: (state, action: PayloadAction<Notification>) => {
+      state.notifications.push(action.payload);
+    },
+    deleteNotification: (state, action: PayloadAction<number>) => {
+      state.notifications = state.notifications.filter((notification) => {
+        return notification.id !== action.payload;
+      });
+    },
+    deleteAllNotifications: (state) => {
+      state.notifications = [];
+    },
+  },
+});
+
+export const { addNotification, deleteNotification, deleteAllNotifications } =
+  notificationsSlice.actions;
+
+export default notificationsSlice.reducer;
