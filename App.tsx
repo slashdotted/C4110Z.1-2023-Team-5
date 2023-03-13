58b4927437a40d6ca7df28066667b5b3dcec22df
filippofinke <filippofinke@protonmail.ch>
2023-03-13 19:07:31 +0100

    feat: make redux storage persistent #22

--- App.tsx
+++ App.tsx
@@ -3,11 +3,12 @@ import { NativeBaseProvider } from "native-base";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Provider } from "react-redux";
-
+import { PersistGate } from "redux-persist/integration/react";
+import FullScreenLoader from "./components/FullScreenLoader";
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
-import store from "./storage/store";
+import store, { persistor } from "./storage/store";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -18,14 +19,16 @@ export default function App() {
   } else {
     return (
       <Provider store={store}>
-        <GestureHandlerRootView style={{ flex: 1 }}>
-          <NativeBaseProvider>
-            <SafeAreaProvider>
-              <Navigation colorScheme={colorScheme} />
-              <StatusBar />
-            </SafeAreaProvider>
-          </NativeBaseProvider>
-        </GestureHandlerRootView>
+        <NativeBaseProvider>
+          <PersistGate loading={<FullScreenLoader />} persistor={persistor}>
+            <GestureHandlerRootView style={{ flex: 1 }}>
+              <SafeAreaProvider>
+                <Navigation colorScheme={colorScheme} />
+                <StatusBar />
+              </SafeAreaProvider>
+            </GestureHandlerRootView>
+          </PersistGate>
+        </NativeBaseProvider>
       </Provider>
     );
   }
--- storage/store.ts
+++ storage/store.ts
@@ -1,15 +1,43 @@
-import { configureStore } from "@reduxjs/toolkit";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from "redux-persist";
 import notificatonsReducer from "./reducers/notificatonsReducer";
 import productsReducer from "./reducers/productsReducer";
 
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+};
+
+const reducer = combineReducers({
+  notifications: notificatonsReducer,
+  products: productsReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
 const store = configureStore({
-  reducer: {
-    notifications: notificatonsReducer,
-    products: productsReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export default store;
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
