d16d134ac8901a5358b02c25e7cb3a2da9eb18e0
filippofinke <filippofinke@protonmail.ch>
2023-03-14 21:45:49 +0100

    feat: add default product

--- storage/reducers/productsReducer.ts
+++ storage/reducers/productsReducer.ts
@@ -7,7 +7,27 @@ export interface ProductsState {
 }
 
 const initialState: ProductsState = {
-  products: [],
+  products: [
+    {
+      barcode: "5449000214911",
+      product_name: "Coca Cola",
+      brands: "Coca Cola",
+      categories: "Beverages",
+      image_url:
+        "https://images.openfoodfacts.org/images/products/544/900/021/4911/front_en.119.400.jpg",
+      ingredients_text:
+        "sugar, water, carbon dioxide, citric acid, phosphoric acid, natural flavors, caffeine",
+      ecoscore_grade: "b",
+      nova_group: 1,
+      nutriscore_grade: "a",
+      nutrient_levels: {
+        fat: "0%",
+        salt: "0%",
+        saturated_fat: "10.6%",
+        sugars: "0%",
+      },
+    },
+  ],
 };
 
 export const productsSlice = createSlice({
@@ -15,7 +35,16 @@ export const productsSlice = createSlice({
   initialState,
   reducers: {
     addProduct: (state, action: PayloadAction<Product>) => {
-      state.products.push(action.payload);
+      // update product if it already exists
+      const index = state.products.findIndex(
+        (product) => product.barcode === action.payload.barcode
+      );
+
+      if (index !== -1) {
+        state.products[index] = action.payload;
+      } else {
+        state.products.push(action.payload);
+      }
     },
   },
 });
