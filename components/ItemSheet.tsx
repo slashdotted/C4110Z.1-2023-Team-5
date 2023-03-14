1731915dd90705b5ae16e44030db2533e3618b54
filippofinke <filippofinke@protonmail.ch>
2023-03-14 20:39:46 +0100

    fix: duplicate key

--- components/ItemSheet.tsx
+++ components/ItemSheet.tsx
@@ -1,7 +1,7 @@
 import { StyleSheet } from "react-native";
 import BottomSheet, {
-    BottomSheetBackdrop,
-    BottomSheetFlatList,
+  BottomSheetBackdrop,
+  BottomSheetFlatList,
 } from "@gorhom/bottom-sheet";
 import { Center, Image, HStack, Box, Button, View } from "native-base";
 import { SvgUri } from "react-native-svg";
@@ -9,176 +9,172 @@ import { Text } from "./Themed";
 import { FridgeItem, Product } from "../constants/Types";
 
 interface ItemSheetProps {
-    onClose: () => void;
-    product: Product;
-    onAddItem?: (product: Product) => void;
-    onContinue?: () => void;
-    onDeleteItem?: (item: Product) => void;
+  onClose: () => void;
+  product: Product;
+  onAddItem?: (product: Product) => void;
+  onContinue?: () => void;
+  onDeleteItem?: (item: Product) => void;
 }
 
 export default function ItemSheet({
-    onClose,
-    product,
-    onAddItem,
-    onContinue,
-    onDeleteItem,
+  onClose,
+  product,
+  onAddItem,
+  onContinue,
+  onDeleteItem,
 }: ItemSheetProps) {
-    const ingredients = [...new Set(product.ingredients_text.split(","))].map(
-        (i) => i.trim()
-    );
+  const ingredients = [
+    ...new Set(product.ingredients_text.split(",").map((i) => i.trim())),
+  ];
 
-    return (
-        <>
-            <BottomSheet
-                backgroundStyle={{
-                    backgroundColor: "rgb(240,240,240)",
-                }}
-                backdropComponent={(props) => {
-                    return (
-                        <BottomSheetBackdrop
-                            disappearsOnIndex={-1}
-                            {...props}
-                            pressBehavior={"close"}
-                        />
-                    );
-                }}
-                enablePanDownToClose={true}
-                snapPoints={["65%", "99%"]}
-                index={0}
-                onClose={onClose}
-                style={{
-                    padding: 20,
-                }}
-            >
-                <Center mb={2}>
-                    <Text style={styles.title}>{product.product_name}</Text>
-                </Center>
-                <Center
-                    backgroundColor={"white"}
-                    borderRadius={20}
-                    maxHeight={180}
-                    padding={5}
-                >
-                    <Image
-                        src={product.image_url}
-                        alt={product.product_name}
-                        resizeMode={"contain"}
-                        style={{
-                            height: "100%",
-                            width: "100%",
-                        }}
-                    />
-                </Center>
+  return (
+    <>
+      <BottomSheet
+        backgroundStyle={{
+          backgroundColor: "rgb(240,240,240)",
+        }}
+        backdropComponent={(props) => {
+          return (
+            <BottomSheetBackdrop
+              disappearsOnIndex={-1}
+              {...props}
+              pressBehavior={"close"}
+            />
+          );
+        }}
+        enablePanDownToClose={true}
+        snapPoints={["65%", "99%"]}
+        index={0}
+        onClose={onClose}
+        style={{
+          padding: 20,
+        }}
+      >
+        <Center mb={2}>
+          <Text style={styles.title}>{product.product_name}</Text>
+        </Center>
+        <Center
+          backgroundColor={"white"}
+          borderRadius={20}
+          maxHeight={180}
+          padding={5}
+        >
+          <Image
+            src={product.image_url}
+            alt={product.product_name}
+            resizeMode={"contain"}
+            style={{
+              height: "100%",
+              width: "100%",
+            }}
+          />
+        </Center>
 
-                <HStack justifyContent={"space-between"} my={3}>
-                    <SvgUri
-                        width={100}
-                        height={60}
-                        viewBox="0 0 240 130"
-                        uri={`https://static.openfoodfacts.org/images/attributes/nutriscore-${
-                            product.nutriscore_grade || "unknown"
-                        }.svg`}
-                    />
+        <HStack justifyContent={"space-between"} my={3}>
+          <SvgUri
+            width={100}
+            height={60}
+            viewBox="0 0 240 130"
+            uri={`https://static.openfoodfacts.org/images/attributes/nutriscore-${
+              product.nutriscore_grade || "unknown"
+            }.svg`}
+          />
 
-                    <SvgUri
-                        width={100}
-                        height={60}
-                        viewBox="0 0 240 130"
-                        uri={`https://static.openfoodfacts.org/images/attributes/ecoscore-${
-                            product.ecoscore_grade || "unknown"
-                        }.svg`}
-                    />
+          <SvgUri
+            width={100}
+            height={60}
+            viewBox="0 0 240 130"
+            uri={`https://static.openfoodfacts.org/images/attributes/ecoscore-${
+              product.ecoscore_grade || "unknown"
+            }.svg`}
+          />
 
-                    <SvgUri
-                        width={100}
-                        height={60}
-                        viewBox="0 0 68 130"
-                        uri={`https://static.openfoodfacts.org/images/attributes/nova-group-${
-                            product.nova_group || "unknown"
-                        }.svg`}
-                    />
-                </HStack>
+          <SvgUri
+            width={100}
+            height={60}
+            viewBox="0 0 68 130"
+            uri={`https://static.openfoodfacts.org/images/attributes/nova-group-${
+              product.nova_group || "unknown"
+            }.svg`}
+          />
+        </HStack>
 
-                <Text style={styles.title}>Ingredients</Text>
-                <Box
-                    my={2}
-                    style={{
-                        flex: 1,
-                        padding: 10,
-                        borderRadius: 20,
-                        marginBottom: 100,
-                    }}
-                    backgroundColor={"white"}
-                >
-                    <BottomSheetFlatList
-                        data={ingredients}
-                        renderItem={({ item }) => (
-                            <Text style={styles.ingredient}>{item}</Text>
-                        )}
-                        keyExtractor={(item) => item}
-                        ListFooterComponent={<View />}
-                    />
-                </Box>
-
-                {onDeleteItem && (
-                    <HStack
-                        style={{
-                            position: "absolute",
-                            justifyContent: "space-around",
-                            paddingBottom: 40,
-                            paddingTop: 6,
-                            bottom: 0,
-                            backgroundColor: "rgb(240,240,240)",
-                            width: "100%",
-                        }}
-                    >
-                        <Button
-                            variant={"outline"}
-                            colorScheme={"red"}
-                            color={"red"}
-                            onPress={() => {
-                                if (onDeleteItem) {
-                                    onDeleteItem(product);
-                                }
-                            }}
-                        >
-                            Delete from fridge
-                        </Button>
-                    </HStack>
-                )}
-            </BottomSheet>
-            {onAddItem && onContinue && (
-                <HStack
-                    style={{
-                        position: "absolute",
-                        justifyContent: "space-around",
-                        paddingBottom: 20,
-                        paddingTop: 6,
-                        bottom: 0,
-                        backgroundColor: "rgb(240,240,240)",
-                        width: "100%",
-                    }}
-                >
-                    <Button
-                        variant={"outline"}
-                        colorScheme={"blue"}
-                        onPress={onContinue}
-                    >
-                        Continue
-                    </Button>
-                </HStack>
+        <Text style={styles.title}>Ingredients</Text>
+        <Box
+          my={2}
+          style={{
+            flex: 1,
+            padding: 10,
+            borderRadius: 20,
+            marginBottom: 100,
+          }}
+          backgroundColor={"white"}
+        >
+          <BottomSheetFlatList
+            data={ingredients}
+            renderItem={({ item }) => (
+              <Text style={styles.ingredient}>{item}</Text>
             )}
-        </>
-    );
+            keyExtractor={(item) => item}
+            ListFooterComponent={<View />}
+          />
+        </Box>
+
+        {onDeleteItem && (
+          <HStack
+            style={{
+              position: "absolute",
+              justifyContent: "space-around",
+              paddingBottom: 40,
+              paddingTop: 6,
+              bottom: 0,
+              backgroundColor: "rgb(240,240,240)",
+              width: "100%",
+            }}
+          >
+            <Button
+              variant={"outline"}
+              colorScheme={"red"}
+              color={"red"}
+              onPress={() => {
+                if (onDeleteItem) {
+                  onDeleteItem(product);
+                }
+              }}
+            >
+              Delete from fridge
+            </Button>
+          </HStack>
+        )}
+      </BottomSheet>
+      {onAddItem && onContinue && (
+        <HStack
+          style={{
+            position: "absolute",
+            justifyContent: "space-around",
+            paddingBottom: 20,
+            paddingTop: 6,
+            bottom: 0,
+            backgroundColor: "rgb(240,240,240)",
+            width: "100%",
+          }}
+        >
+          <Button variant={"outline"} colorScheme={"blue"} onPress={onContinue}>
+            Continue
+          </Button>
+        </HStack>
+      )}
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-    title: {
-        fontSize: 22,
-        fontWeight: "bold",
-    },
-    ingredient: {
-        fontSize: 18,
-        marginVertical: 2,
-    },
+  title: {
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+  ingredient: {
+    fontSize: 18,
+    marginVertical: 2,
+  },
 });
