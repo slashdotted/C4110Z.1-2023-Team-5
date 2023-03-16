35dfe25ba0fcbe0085fe6906e2a3267811151a29
filippofinke <filippofinke@protonmail.ch>
2023-03-16 10:49:37 +0100

    fix: code style

--- components/FridgeListItem.tsx
+++ components/FridgeListItem.tsx
@@ -8,94 +8,86 @@ import expirationLevel from "../utils/expirationLevel";
 import { VStack, Image } from "native-base";
 
 type FridgeItemProps = {
-    item: FridgeItem;
-    onPress: (item: FridgeItem) => void;
-    onLongPress: (item: FridgeItem) => void;
-    onDelete: (item: FridgeItem) => void;
-    selected: boolean;
+  item: FridgeItem;
+  onPress: (item: FridgeItem) => void;
+  onLongPress: (item: FridgeItem) => void;
+  onDelete: (item: FridgeItem) => void;
+  selected: boolean;
 };
 
 export default function FridgeListItem({
-    item,
-    onPress,
-    onLongPress,
-    onDelete,
-    selected,
+  item,
+  onPress,
+  onLongPress,
+  onDelete,
+  selected,
 }: FridgeItemProps) {
-    let style = {
-        borderColor: "rgba(185,28,28,0.4)"
-    };
-    let icon = { name: "exclamation", color: "red" };
+  let style = {
+    borderColor: "rgba(185,28,28,0.4)",
+  };
+  let icon = { name: "exclamation", color: "red" };
 
-    var level = expirationLevel(item);
+  var level = expirationLevel(item);
 
-    if (level == 2) {
-        style.borderColor = "rgba(255,153,0,0.4)";
-        icon.name = "warning";
-        icon.color = "#FF9900";
-    } else if (level == 3) {
-        style.borderColor = "rgba(21,128,61,0.4)";
-        icon.name = "check-square-o";
-        icon.color = "#15803D";
-    }
+  if (level == 2) {
+    style.borderColor = "rgba(255,153,0,0.4)";
+    icon.name = "warning";
+    icon.color = "#FF9900";
+  } else if (level == 3) {
+    style.borderColor = "rgba(21,128,61,0.4)";
+    icon.name = "check-square-o";
+    icon.color = "#15803D";
+  }
 
-    return (
-        
-            <Pressable
-                onPress={() => {
-                    onPress(item);
-                }}
-                onLongPress={() => {
-                    onLongPress(item);
-                }}
-                style={[styles.item, style, selected && { opacity: 0.5 }]}
-            >
-                <VStack space={1.5} alignItems="center">
-                    <Image
-                        source={{
-                            uri: item.product.image_url,
-                        }}
-                        alt="Alternate Text"
-                        size={"xl"}
-                        resizeMode="contain"
-                    />
-                    <Text
-                        numberOfLines={1}
-                        ellipsizeMode="tail"
-                        style={styles.title}
-                    >
-                        {item.product.product_name}
-                    </Text>
+  return (
+    <Pressable
+      onPress={() => {
+        onPress(item);
+      }}
+      onLongPress={() => {
+        onLongPress(item);
+      }}
+      style={[styles.item, style, selected && { opacity: 0.5 }]}
+    >
+      <VStack space={1.5} alignItems="center">
+        <Image
+          source={{
+            uri: item.product.image_url,
+          }}
+          alt="Alternate Text"
+          size={"xl"}
+          resizeMode="contain"
+        />
+        <Text numberOfLines={1} ellipsizeMode="tail" style={styles.title}>
+          {item.product.product_name}
+        </Text>
 
-                    <Text style={styles.expDate}>
-                        <FontAwesome 
-                            name={icon.name as any}
-                            color={icon.color}
-                        />
-                        
-                        {"  " + new Date(item.expirationDate).toLocaleDateString()}
-                    </Text>
-                </VStack>
-            </Pressable>
-    );
+        <Text style={styles.expDate}>
+          <FontAwesome name={icon.name as any} color={icon.color} />
+
+          {"  " + new Date(item.expirationDate).toLocaleDateString()}
+        </Text>
+      </VStack>
+    </Pressable>
+  );
 }
 
 const styles = StyleSheet.create({
-    title: {
-        fontSize: 20,
-        fontWeight: "bold",
-    },
-    item: {
-        borderWidth: 1,
-        padding: 10,
-        borderRadius: 15,
-        justifyContent: "space-between", 
-        flex: 1,
-        margin: 5, 
-    },
-    expDate: {
-        alignContent: "flex-end",
-        marginBottom: 10,
-        color: "#404040"
-    },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  item: {
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 15,
+    justifyContent: "space-between",
+    flex: 1,
+    margin: 5,
+  },
+  expDate: {
+    alignContent: "flex-end",
+    marginBottom: 10,
+    color: "#404040",
+  },
 });
