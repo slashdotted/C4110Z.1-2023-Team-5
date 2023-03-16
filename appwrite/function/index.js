0c93aeac621b11a373ee65f39cf16e41716ef486
filippofinke <filippofinke@protonmail.ch>
2023-03-16 13:10:03 +0100

    feat(function): add example function #24

---
+++ appwrite/function/index.js
@@ -0,0 +1,16 @@
+/*
+  'req' variable has:
+    'headers' - object with request headers
+    'payload' - object with request body data
+    'env' - object with environment variables
+  'res' variable has:
+    'send(text, status)' - function to return text response. Status code defaults to 200
+    'json(obj, status)' - function to return JSON response. Status code defaults to 200
+  If an error is thrown, a response with code 500 will be returned.
+*/
+
+module.exports = async function (req, res) {
+  res.json({
+    message: "Hello World!",
+  });
+};
